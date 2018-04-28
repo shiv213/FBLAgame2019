ec63f34132ce66447294b0b15cca77119dc75cc7
Yaseen <9275716+ynx0@users.noreply.github.com>
2018-04-27 20:29:01 -0400

    Add basic game classes and fix game loop
    Signed-off-by: Yaseen Shaikh <yaseentheepic@gmail.com>

--- res/js/game.js
+++ res/js/game.js
@@ -14,9 +14,108 @@ const app = new PIXI.Application({
     height: size.height * (SCALE - 0.00)
 });
 
+// MARK - Helper functions
 
-// credit http://pixeljoint.com/pixelart/46064.htm
+function contain(sprite, container) {
+    let collision = undefined;
+    //Left
+    if (sprite.x < container.x) {
+        sprite.x = container.x;
+        collision = "left";
+    }
+    //Top
+    if (sprite.y < container.y) {
+        sprite.y = container.y;
+        collision = "top";
+    }
+    //Right
+    if (sprite.x + sprite.width > container.width) {
+        sprite.x = container.width - sprite.width;
+        collision = "right";
+    }
+    //Bottom
+    if (sprite.y + sprite.height > container.height) {
+        sprite.y = container.height - sprite.height;
+        collision = "bottom";
+    }
+    //Return the `collision` value
+    return collision;
+}
 
+// MARK - Game classes
+class EnemyShip {
+
+    constructor(texture, damage) {
+        this.damage = damage;
+        // noinspection JSAccessibilityCheck
+        this._sprite = new PIXI.Sprite(PIXI.utils.TextureCache[texture]);
+    }
+
+    moveX(amt) {
+        this._sprite.vx = amt;
+        this._sprite.x += this._sprite.vx;
+    }
+
+    moveY(amt) {
+        this._sprite.vy = amt;
+        this._sprite.y += this._sprite.vy;
+    }
+
+    moveAI() {
+        let upOrDown = Math.random() < 0.5 ? -1 : 1; // move up or down in y
+        this.moveY((Math.floor(Math.random() * 4) + 1) * upOrDown);
+
+    }
+
+    /**
+     *
+     * @returns Bullet
+     */
+    shootAI() {
+        let shouldShoot = Boolean(Math.floor(Math.random() * 2));
+        if (shouldShoot) {
+            return this.shootBullet();
+        }
+    }
+
+    shootBullet() {
+        return new Bullet(null, this.damage);
+    }
+
+}
+
+class Bullet {
+    constructor(texture, damage) {
+        this.damage = damage || 5;
+        this.tickCreated = null;
+        if (!texture) {
+            texture = "bullet1";
+        }
+        this.dirty = false;
+
+        // noinspection JSAccessibilityCheck
+        this._sprite = new PIXI.Sprite(PIXI.utils.TextureCache[texture])
+    }
+
+    move(amt) {
+        this._sprite.vx = -amt; // it will keep moving left
+        this._sprite.x += this._sprite.vx;
+    }
+
+    clean(tick) {
+        if (!this.tickCreated) {
+            this.tickCreated = tick;
+        } else if ((tick - this.tickCreated) > 10000) {
+            app.stage.remove(this._sprite);
+            self.dirty = true;
+        }
+    }
+}
+
+/* credit:
+    http://pixeljoint.com/pixelart/46064.htm
+    http://pixeljoint.com/pixelart/35997.htm
+*/
 // document.addEventListener("DOMContentLoaded", () => );
 
 // TODO Make mixed tiles using groups and then make a final tile
@@ -24,7 +123,7 @@ const app = new PIXI.Application({
 // load fonts
 WebFont.load({
     google: {
-        families: ["Press Start 2P", "Pixelar"]
+        families: ["Pixelar"]
     },
     active: function () {
         init()
@@ -42,6 +141,7 @@ function init() {
         .add('bullet1', 'img/bullet1.png')  // 5 health
         .add('bullet2', 'img/bullet2.png') //10 health
         .add('play_btn', 'img/play.png')
+        .add('arthur_ship', 'img/arthur.png')
         .add('info_btn', 'img/info.png')
         // fonts
         // .add('Upheaval', 'font/upheaval/upheaval.ttf')
@@ -52,9 +152,9 @@ function init() {
 function setup(loader, resources) {
 
     // MARK - Main variables
-    let started  = false;
+    let started = false;
     let gameOver = false;
-
+    let relTick;
 
     // MARK - fonts
     let splashTextStyle = new PIXI.TextStyle({
@@ -65,19 +165,23 @@ function setup(loader, resources) {
     });
 
     let subtitleStyle = new PIXI.TextStyle({
-        fontFamily: "Press Start 2P"
+        fontFamily: "Press Start 2P",
+        fontSize: 20,
+        fill: "#efefef"
     });
 
     let regularTextStyle = new PIXI.TextStyle({
-        fontFamily: "VCR OSD Mono"
+        fontFamily: "VCR OSD Mono",
+        fontSize: 42,
+        fill: "#6fa0a7",
+        align: "left"
     });
 
     // MARK - buttons
-    var playBtn = new PIXI.Sprite(resources.play_btn.texture);
-    var infoBtn = new PIXI.Sprite(resources.info_btn.texture);
-    var btns = [playBtn, infoBtn];
+    let playBtn = new PIXI.Sprite(resources.play_btn.texture);
+    let infoBtn = new PIXI.Sprite(resources.info_btn.texture);
+    let btns = [playBtn, infoBtn];
 
-    let btnScale = 5;
 
     let startBtnOffsetX = 200;
     let startBtnOffsetY = 0;
@@ -85,10 +189,7 @@ function setup(loader, resources) {
     let infoBtnOffsetX = -300;
     let infoBtnOffsetY = 40;
 
-    function btnDown() {
-        console.log("Btn down", this);
-        started = true;
-    }
+
     btns.forEach((btn) => {
         btn.buttonMode = true;
         btn.interactive = true;
@@ -109,13 +210,13 @@ function setup(loader, resources) {
         console.log(started);
     });
     infoBtn.on('pointerdown', () => {
-        console.log("Infooo")
+        console.log("Infooo");
     });
 
 
     // MARK - Background
     const bg = new PIXI.extras.TilingSprite(resources.bg_tile.texture, app.screen.width, app.screen.height);
-    const bg_accel_rate = 0.005;
+    const bg_accel_rate = 0.010;
     const bg_static = 10;
     let bg_delta;
 
@@ -137,20 +238,22 @@ function setup(loader, resources) {
     // MARK - Alternate Stages
 
     // GAME OVER (only display on end, then after x amount of time, go back to beginning
+    let gameOverDelay = 75; // ticks
     let gameOverStage = new PIXI.Container();
     gameOverStage.addChild(bg);
     let gameOverText = new PIXI.Text("GAME\nOVER", splashTextStyle);
     gameOverText.anchor.set(0.5, 0.5);
     gameOverText.position.set(app.screen.width / 2, app.screen.height / 2);
     gameOverStage.addChild(gameOverText);
+    gameOverStage.visible = false;
     // GAME OVER
 
     // INFO
     let infoStage = new PIXI.Container();
     infoStage.addChild(bg);
     infoStage.addChild(infoText);
+    infoStage.visible = false;
     // INFO
-
 
 
     // MARK - add all elements
@@ -158,31 +261,53 @@ function setup(loader, resources) {
     app.stage.addChild(splashText);
     app.stage.addChild(playBtn);
     app.stage.addChild(infoBtn);
+    app.stage.addChild(gameOverStage);
+    app.stage.addChild(infoStage);
 
+
+    // // debug
+    // if(tick > 300 && started) {
+    //     gameOver = true;
+    // }
 
 
     // Main game loop
     app.ticker.add(() => {
         tick++;
 
-        // debug
-        if(tick > 300 && started) {
-            gameOver = true;
-        }
+        // contain all sprites in canvas
+        contain()
+
 
         // MARK - Background processing
         bg_delta = bg_accel_rate * tick;
         if (!started) {
             bg.tilePosition.x += -bg_static;
+        } else if (gameOver) {
+            // don't move the bg
+            // bg.tilePosition.x = 0;
         } else {
             bg.tilePosition.x += -bg_delta;
         }
 
-        if(gameOver) {
-            // reset everything
-            tick = 0;
-            started = false;
-            gameOver = false;
+
+        if (gameOver) {
+            if (!relTick) {
+                relTick = tick; // takes snapshot of tick for comparison
+            }
+            console.log("rel tick", relTick);
+            // we are done, wait for gameOver stage to finish
+            if ((tick - relTick) >= gameOverDelay) {
+                // reset everything after game over delay
+                tick = 0;
+                relTick = null;
+                started = false;
+                gameOver = false;
+                gameOverStage.visible = false;
+            } else {
+                gameOverStage.visible = true;
+            }
+
         }
 
         // MARK - splash text processing
@@ -192,7 +317,7 @@ function setup(loader, resources) {
             btns.forEach((b) => {
                 b.visible = false; //todo make visible again
             })
-        } else if(!gameOver) {
+        } else if (!gameOver) {
             // console.log("Not started");
             splashText.visible = true; // todo test / delay
             btns.forEach((b) => {
